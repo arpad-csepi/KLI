deb77670e31e5647db1e251dd29090fe2305a758
Árpád Csepi <csepi.arpad@outlook.com>
2022-10-29 19:57:31 +0000

    Local flag added

--- cmd/install.go
+++ cmd/install.go
@@ -5,6 +5,8 @@ Copyright © 2022 Árpád Csepi csepi.arpad@outlook.com
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 
 	"flag"
@@ -22,17 +24,35 @@ var installCmd = &cobra.Command{
 	Short: "Install a resource",
 	Long:  `TODO`,
 	Run: func(cmd *cobra.Command, args []string) {
+
+		switch resource_name, _ := cmd.Flags().GetString("resource"); resource_name {
+		case "istio-operator":
 			var (
-				chartUrl = "https://kubernetes-charts.banzaicloud.com"
-				repositoryName    = "banzaicloud-stable"
-				chartName   = "istio-operator"
-				releaseName = "banzaicloud-stable"
-				namespace   = "istio-system"
+				chartUrl       = "https://kubernetes-charts.banzaicloud.com"
+				repositoryName = "banzaicloud-stable"
+				chartName      = "istio-operator"
+				releaseName    = "banzaicloud-stable"
+				namespace      = "istio-system"
 			)
 
-		kubereflex.InstallHelmChart(chartUrl, repositoryName, chartName, releaseName, namespace, nil, getKubeConfig())
+			kubereflex.InstallHelmChart(chartUrl, repositoryName, chartName, releaseName, namespace, nil, getKubeConfig())
+		case "cluster-registry":
+			var (
+				chartUrl       = "https://cisco-open.github.io/cluster-registry-controller"
+				repositoryName = "cluster-registry"
+				chartName      = "cluster-registry"
+				releaseName    = "cluster-registry"
+				namespace      = "cluster-registry"
+			)
+
+			kubereflex.InstallHelmChart(chartUrl, repositoryName, chartName, releaseName, namespace, nil, getKubeConfig())
+		default:
+			fmt.Printf("Unknown resource")
+		}
 	},
 }
+
+var resource string
 
 func init() {
 	rootCmd.AddCommand(installCmd)
@@ -48,6 +68,9 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// installCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	installCmd.Flags().StringVarP(&resource, "resource", "r", "", "Resource is required")
+	installCmd.MarkFlagRequired("resource")
 }
 
 func getKubeConfig() *string {
